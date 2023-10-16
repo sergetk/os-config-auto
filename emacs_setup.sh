9ce1b503d202b20186a89faacc381643f92e986b
sergetk <tkatchserg@gmail.com>
2023-10-16 15:17:15 +1300

    added sudo to nvm and npm commands

--- emacs_setup.sh
+++ emacs_setup.sh
@@ -8,6 +8,6 @@ cat pass.txt | sudo -S sh -c "yes | pacman -S emacs"
 (echo "# short-cut to start emacs " ; echo bindsym \$mod+Ctrl+Escape exec --no-startup-id emacsclient -e '(kill emacs)' ; echo "")>> $HOME/.i3/config
 (echo "# start emacs as daemon " ; echo exec_always --no-startup-id emacs --daemon ; echo "")>> $HOME/.i3/config
 
-nvm install --lts
-npm install -g bash-language-server
+cat pass.txt | sudo -S sh -c "nvm install --lts"
+cat pass.txt | sudo -S sh -c "npm install -g bash-language-server"
 cat pass.txt | sudo -S sh -c "yes | pacman -S shellcheck"
