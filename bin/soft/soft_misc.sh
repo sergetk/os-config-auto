e41915b28ac7d82e8d7fd9a9b578ccc89264d2fa
sergetk <tkatchserg@gmail.com>
2025-03-30 14:10:10 +1300

    added nordvpn install

--- bin/soft/soft_misc.sh
+++ bin/soft/soft_misc.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #
-#: Title	: software_setup 
+#: Title	: software_setup
 #: Author	: stk
-#: Description	: install software 
+#: Description	: install software
 #: Version 	: 0.1
 #: Options 	: none
 #
@@ -10,8 +10,7 @@
 absPath="${PWD%%os-config-auto*}os-config-auto"
 . "${absPath}/bin/utils/util_functions.sh"
 
-append_nvm_to_shellrc() #@ append nvm path to shell rc file: $1 shell rc file path
-{
+append_nvm_to_shellrc() { #@ append nvm path to shell rc file: $1 shell rc file path
   # shellcheck disable=SC2016
   append_to 'export NVM_DIR="$HOME/.nvm"' "$1"
   append_to '[ -s "/usr/share/nvm/init-nvm.sh" ] && . "/usr/share/nvm/init-nvm.sh"' "$1"
@@ -24,11 +23,11 @@ installMisc() {
   cp "$HOME/os-config-auto/scripts/wallpaper.sh" "$HOME/.config/"
   append_to_i3 "# load wallpaper script " "exec_always --no-startup-id sh ~/.config/wallpaper.sh"
   mv "$HOME/.config/nitrogen" "$HOME/.config/nitro"
-  
-  y_install keepass 
+
+  y_install keepass
   y_install unrar
 
-  sed -i 's/bottom/top/g' ~/.i3/config 
+  sed -i 's/bottom/top/g' ~/.i3/config
 
   #installing brave browser
   sudo_cmd "sed -i 's/^#EnableAUR/EnableAUR/' /etc/pamac.conf"
@@ -40,7 +39,7 @@ installMisc() {
   y_install zsh
 
   #installing node version manager
-  y_install nvm 
+  y_install nvm
 
   curl -fsSL -o install_ohmyzsh.sh https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
   sh install_ohmyzsh.sh --unattended
@@ -50,7 +49,7 @@ installMisc() {
 
   export NVM_DIR="$HOME/.nvm"
   # shellcheck source=/dev/null
-  source /usr/share/nvm/init-nvm.sh 
+  source /usr/share/nvm/init-nvm.sh
 
   nvm install node
   nvm use node
@@ -59,4 +58,12 @@ installMisc() {
   y_install dart
   #shellcheck disable=SC2016
   append_to 'export PATH="$PATH:/usr/lib/dart/bin"' "$HOME/.profile"
+
+  #shellcheck disable=SC2164
+  cd "$HOME/nordvpn"
+  makepkg -fs
+  sudo_cmd "pacman -U nordvpn-bin-*"
+  sudo_cmd "groupadd nordvpn"
+  sudo_cmd "usermod -aG nordvpn $USER"
+  sudo_cmd "systemctl enable --now nordvpnd"
 }
